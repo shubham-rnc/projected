9711a35089470e9b5fb28f4120c92f5be807d21f
Shubham Choudhary <shubham.rnc@gmail.com>
2015-02-15 03:19:39 +0530

    Remember me complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209155002) do
+ActiveRecord::Schema.define(version: 20150214192327) do
 
   create_table "allowedvalueslevels", force: :cascade do |t|
     t.string   "values"
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 20150209155002) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
