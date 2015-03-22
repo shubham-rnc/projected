031f0cc79642ecb138a1027560f0dbd0a0562f9c
shubham choudhary <shubham.rnc@gmail.com>
2015-03-22 16:08:19 +0530

    autocomplete done

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
   get 'institutepage/:slug' => 'institutepage#show'
   # You can have the root of your site routed with "root"
   root 'home#index'
-
+ 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
